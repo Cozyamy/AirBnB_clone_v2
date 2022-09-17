9144664e4af47185a0b56836bea6566d633a7af6
Boye <adeboyeakingbelure18@yahoo.com>
2022-09-16 19:29:02 -0700

    databases

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,8 +7,8 @@
 -- If the database hbnb_dev_db or the user hbnb_dev already exists,
 -- + your script should not fail
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,8 +7,8 @@
 -- If the database hbnb_test_db or the user hbnb_test already exists,
 -- + your script should not fail
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
